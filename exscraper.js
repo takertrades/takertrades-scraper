5d97d916b7a267ee4e69a9af0970916cea046119
Todd Christensen <christensen.tr@gmail.com>
2020-01-14 03:17:49 +0800

    uploading test scraper

---
+++ exscraper.js
@@ -0,0 +1,11 @@
+const puppeteer = require("puppeteer");
+async function run() {
+  const browser = await puppeteer.launch({
+    defaultViewport: { width: 1920, height: 1080 }
+  });
+  const page = await browser.newPage();
+  await page.goto("https://www.google.com/search?q=bitcoin");
+  await page.screenshot({ path: "screenshot.png" });
+  browser.close();
+}
+run();
